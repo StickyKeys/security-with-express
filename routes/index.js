c38002acdc2bd15e7b7f860ab40b7da4b5105c29
Steven Bartkiewicz <sbartkiewicz@gmail.com>
2024-06-19 23:09:58 -0400

    Made a nice sign in page to use as a template for other programs

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  let response = "<h3>Home Page</h3>";
-  response += `<small>${req.requestTime}</small>`;
-  res.send(response);
+  // let response = "<h3>Home Page</h3>";
+  // response += `<small>${req.requestTime}</small>`;
+  // res.send(response);
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 module.exports = router;
